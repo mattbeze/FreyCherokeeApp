2508f94e123aabe50675a7f77f71978a07522c4d
Mattheus Bezerra <43768504+mattbeze@users.noreply.github.com>
2019-03-28 14:16:31 -0400

    Web Pages
    All web pages as of 3/28

---
+++ src/app/app/Feedback/Feedback.page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-Feedback',
+  templateUrl: 'Feedback.page.html',
+  styleUrls: ['Feedback.page.scss']
+})
+export class FeedbackPage {}
